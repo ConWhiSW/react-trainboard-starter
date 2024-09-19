e406a88b60431e9acc529f3aee5d603a6956e1f2
Dan Wilkins <danwilkins2003@gmail.com>
2024-09-19 16:07:29 +0100

    Updated Routing System

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return <div>
         <h1>Trainboard Train Station Provider</h1>
         <h2>This is the home/landing page</h2>
+        <Link to="/ABC123U&ME">Stations</Link>
+
     </div>;
 };
 
