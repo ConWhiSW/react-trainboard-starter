ece9089aa4bd5588f214d6296da227ba2a4267b9
ConWhiSW <connor.whitworth@softwire.com>
2024-09-20 11:23:04 +0100

    DisplayRow not necessary - Display as table on StationPage

--- src/components/pages/Station.tsx
+++ src/components/pages/Station.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { format } from 'date-fns';
 import { faresData, journeyDetail, stationDetail } from '../../customTypes';
 import { fetchRoutes } from '../../helpers/ApiCallHelper';
 import { organiseResponse } from '../../helpers/HandleApiResponse';
-import DisplayRow from '../displayrow/DisplayRow';
 
 const Station: React.FC = () => {
 
@@ -17,6 +17,12 @@ const Station: React.FC = () => {
             })
             .catch((err) => setResponse('There has been an error whilst finding your route. Error: ' + err));
     }, []);
+
+    const minutesToHours = (minutes: number) => {
+        const fullHours = Math.floor((minutes / 60));
+        const remainingMinutes = minutes % 60;
+        return fullHours + 'hr ' + remainingMinutes + 'min';
+    };
 
     const exampleApiRequest : faresData = {
         originStation : from!,
@@ -45,10 +51,46 @@ const Station: React.FC = () => {
                     {' to '}
                     {response[0].destinationStation.displayName}
                 </h2>
-                {response.map((route) => {return (<DisplayRow
-                    key={response.indexOf(route)} route={route} />);})}
+                <table>
+                    <tr>
+                        <th>From</th>
+                        <th>To</th>
+                        <th>Departure</th>
+                        <th>Arrival</th>
+                        <th>Status</th>
+                        <th>Duration</th>
+                    </tr>
+                    {response.map((route) => {
+                        let status = route.status;
+                        if (route.status == 'normal') {
+                            status = 'On Time';
+                        }
+                        return (
+                            <tr key={response.indexOf(route)}>
+                                <td>
+                                    {route.originStation.displayName}
+                                </td>
+                                <td>
+                                    {route.destinationStation.displayName}
+                                </td>
+                                <td>
+                                    {format(route.departureTime, 'dd/MM/yyyy HH:mm')}
+                                </td>
+                                <td>
+                                    {format(route.arrivalTime, 'dd/MM/yyyy HH:mm')}
+                                </td>
+                                <td>
+                                    {status}
+                                </td>
+                                <td>
+                                    {minutesToHours(route.journeyDurationInMinutes)}
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </table>
             </div>
-        )
+        );
     }
 };
 
